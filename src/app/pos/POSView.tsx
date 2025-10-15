6d6e5f024fae1bd2929d0b1891451d05f3531845
Prajwal J <prajwal.workmail@gmail.com>
2025-10-15 09:09:22 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/pos/POSView.tsx
+++ src/app/pos/POSView.tsx
@@ -66,7 +66,7 @@ export default function POSView() {
                     <Button onClick={() => handlePrintBill(order)} className="w-full">
                       <Printer className="mr-2 h-4 w-4" />
                       Print Bill
-                    Button>
+                    </Button>
                   )}
                 </div>
               </OrderCard>
